19a3206783b7be7eded9592045e4857b46782dec
shelllbelll <109366658+shelllbelll@users.noreply.github.com>
2024-09-24 14:51:36 -0500

    dictionary made for df, lookup for data analysis made easier

--- python_playgrounds/UpdatedGSA.py
+++ python_playgrounds/UpdatedGSA.py
@@ -15,8 +15,8 @@ def custom_get(*args, **kwargs):
 r.get = custom_get #replace with custom get
 
 #define vars
-period = '1d'
-tickerList = ["AMZN"]
+period = '5d'
+tickerList = ["AMZN", "NTDOY"]
 ReqRate = [4,Duration.SECOND * 5]
 
 #creates cached sessions to ease amount of yahoo requests
@@ -34,21 +34,17 @@ try:
     df = pd.concat([yf.Ticker(ticker).history(period=period).assign(
             ticker=ticker
             ) for ticker in tickerList], 
-            ignore_index=True)
+            ignore_index=True) #removes dates, replaces w ints [0,i]
     
 except Exception as e:
     print(f"error dowloading data: {e}")
 
 r.get = original_get #replace the original get to continue w yf
 
-print(df.head()) #check df
+#dictionary with tickers as keys, subcols (eg. 'Open', 'Close', etc.) as vals (list)
+d_ticker = {
+    ticker: gp.drop(columns='ticker').to_dict(orient='list') for ticker, 
+    gp in df.groupby('ticker')
+}
 
-#parse through df and separate columns
-#dictionary for df
-#if multiple tickers, receiving data from each subcol would need separation
-#if single ticker, index is subcol (eg. 'Close', 'Open', etc.)
-
-d = {idx: gp.T for idx,
-     gp in df.T.groupby(level=0) 
-     }
-
+print(d_ticker['AMZN']['Open'])
