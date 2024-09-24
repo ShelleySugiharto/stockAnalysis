cca8501b96ecdd46b7f05b268de761aad72a18a7
shelllbelll <109366658+shelllbelll@users.noreply.github.com>
2024-09-24 15:48:26 -0500

    user input readability

--- python_playgrounds/generalizedStockAnalysis.py
+++ python_playgrounds/generalizedStockAnalysis.py
@@ -5,8 +5,9 @@ from requests_cache import CacheMixin, SQLiteCache
 from requests_ratelimiter import LimiterMixin, MemoryQueueBucket
 from pyrate_limiter import Duration, RequestRate, Limiter
 
-#monkey patch the yfinance api for the custom get
+REQ_RATE = [4,Duration.SECOND * 5] #Constant rate for limiter
 
+#monkey patch the yfinance api for the custom get
 original_get = r.get #backup the original get from yf
 
 def custom_get(*args, **kwargs):
@@ -14,17 +15,24 @@ def custom_get(*args, **kwargs):
 
 r.get = custom_get #replace with custom get
 
-#define vars
-period = '5d'
-tickerList = ["AMZN", "NTDOY"]
-ReqRate = [4,Duration.SECOND * 5]
+#user input for period and ticker(s)
+print('Choose from 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max')
+period = input('Enter desired period:')
+tickerList = input('Enter ticker(s) separated by commas, no spaces:')
+
+#make ticker list string into list
+if ',' in tickerList:
+    tickerList = tickerList.split(',')
+
+else:
+    tickerList = [tickerList]
 
 #creates cached sessions to ease amount of yahoo requests
 class CachedLimiterSession(CacheMixin, LimiterMixin, r.Session):
     pass
 
 session = CachedLimiterSession(
-    limiter = Limiter(RequestRate(ReqRate[0], ReqRate[1])),
+    limiter = Limiter(RequestRate(REQ_RATE[0], REQ_RATE[1])),
     bucket_class = MemoryQueueBucket,
     backend = SQLiteCache("yfinance.cache")
 )
@@ -47,4 +55,5 @@ d_ticker = {
     gp in df.groupby('ticker')
 }
 
-print(d_ticker['AMZN']['Open'])
+print(d_ticker)
+
